565ca6e782e8e941047690caa72e1a3db142e75e
Roman Sokolov <gnonasis@gmail.com>
2026-02-18 12:13:56 +0200

    add user register path

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
-from user.views import current_user_page, logout_link
+from user.views import current_user_page, logout_link, RegisterView
+
 
 app_name = "user"
 
@@ -7,5 +8,6 @@ urlpatterns = [
     path('current/', current_user_page, name='user_page'),
     path('', include('django.contrib.auth.urls')),
     path("logout-link/", logout_link, name="logout-link"),
+    path( "register/", RegisterView.as_view(), name="register"),
 
 ]
