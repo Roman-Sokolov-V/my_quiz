af32d5e0bbb547d87032766d108d9bc115517575
Roman Sokolov <gnonasis@gmail.com>
2026-02-18 12:15:10 +0200

    add category creation path

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 
-from quiz.models import Question
-from quiz.views import QuizCreateView, questions_create
+from quiz.views import QuizCreateView, questions_create, CategoryCreateView
 
 
 app_name = 'quiz'
 
 urlpatterns = [
-    path('create/', QuizCreateView.as_view(), name='quiz_create'),
-    path('<int:pk>/question_create/', questions_create, name='question_create'),
+    path('create/', QuizCreateView.as_view(), name='quiz-create'),
+    path('<int:pk>/question_create/', questions_create, name='question-create'),
+    path('category_create/', CategoryCreateView.as_view(), name='category-create'),
+
     #path('<int:pk>/', QuizDetailView.as_view(), name='quiz_detail'),
 
 ]
